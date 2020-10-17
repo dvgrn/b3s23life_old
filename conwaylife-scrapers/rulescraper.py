9e5423b5479246ecb2cd673cf366e009d6952642
Dave Greene <david.m.greene@gmail.com>
2020-10-16 20:59:37 -0500

    Update rulescraper.py to version 1.2

--- conwaylife-scrapers/rulescraper.py
+++ conwaylife-scrapers/rulescraper.py
@@ -1,13 +1,14 @@
 # lifewiki-rulescraper-v1.1.py
 # v1.0: initial working version
 # v1.1: fix problem with names incorrectly containing forward slashes or backslashes
+# v1.2: fix a few limitations of the error-checking and problem-reporting code
 
 import golly as g
 import urllib2
 import os
 
-samplepath = g.getstring("Enter path to save .rule files","C:/users/{username}/Desktop/LWRules/")
-if samplepath == "C:/users/{username}/Desktop/LWRulesz/":
+samplepath = g.getstring("Enter path to save .rule files (scroll left to see full path)","C:/users/{username}/Desktop/LWRules/")
+if samplepath == "C:/users/{username}/Desktop/LWRules/":
   g.note("Please run this script again and change the sample path to something that works on your system.\n\n" \
        + "If the path you supply does not point to folders that you have permission to write to, " \
        + "or if synthfile is not present, the data collection process will eventually fail with an error.")
@@ -71,34 +72,38 @@ for url in linklist: ##############################################
     html=html[end:]
 
 s = ""
+count = 0
 for item in articlelist:
   if item[:6]!="/wiki/":
-    g.note("Weird article link: " +item)
+    s += "Weird article link: " +item +"\n"
     continue
   articlename = item[6:]
   url = 'http://conwaylife.com/w/index.php?title=' + articlename + '&action=edit'
-  response = urllib2.urlopen(url)
-  html = response.read()
+  try:
+    response = urllib2.urlopen(url)
+    html = response.read()
+  except:
+    s += "ERROR READING " + url + "\n"
   begintext = html.find('wpTextbox1">')
   if begintext<0:
-    g.note("Could not find article text textbox 'wpTextbox1' in HTML for " + articlename + ".")
+    s += "Could not find article text textbox 'wpTextbox1' in HTML for " + articlename + "\n"
   else:
     ind = html.find('</textarea>')
     if ind<0:
-      g.note("Could not find /textarea tag at end of textbox in HTML for " + articlename + ".")
+      s += "Could not find /textarea tag at end of textbox in HTML for " + articlename + "\n"
     html = html[begintext+12:ind]
     rulename1 = html.split("\n")[0].replace("@RULE ","").replace("\n","")
     rulename2 = item.replace("/wiki/Rule:","")
-    g.show("Saving " + rulename1)
+    g.show(str(count) + ": Saving " + rulename1 + str(len(s)))
     with open(samplepath + rulename1.replace("/","").replace("\\","") + ".rule","w") as f:
       f.write(html)
     if rulename1 != rulename2:
-      s += rulename1 + " doesn't match " + item + "\n"
+      rulename2 = rulename2[0].lower() + rulename2[1:]
+      if rulename1 != rulename2:
+        s += rulename1 + " doesn't match " + item + "\n"
     if rulename1.find("/")>-1:
-      s += "(THE ABOVE IS A BAD ONE, CONTAINING FORWARD SLASHES)"
+      s += "(THE ABOVE IS A BAD ONE, CONTAINING FORWARD SLASHES)\n\n"
     if rulename1.find("\\")>-1:
-      s += "(THE ABOVE IS A BAD ONE, CONTAINING BACKSLASHES)"
-    
-
+      s += "(THE ABOVE IS A BAD ONE, CONTAINING BACKSLASHES)\n\n"
 g.note("Done.  Click OK to copy list of exceptions to clipboard.")
 g.setclipstr(s)
